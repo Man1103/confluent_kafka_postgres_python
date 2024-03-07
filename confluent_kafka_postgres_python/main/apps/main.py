b47c450f77788059d0f87cd98cf03568c599fc50
Man1103 <mithileshnakade@gmail.com>
2024-03-07 18:06:04 +0530

    full configparser added with function configs

--- confluent_kafka_postgres_python/main/apps/main.py
+++ confluent_kafka_postgres_python/main/apps/main.py
@@ -14,11 +14,14 @@ postgres_port=config.getint('postgres','port')
 postgres_user=config.get('postgres','user').strip()
 postgres_password=config.get('postgres','password').strip()
 postgres_database=config.get('postgres','database').strip()
+print(postgres_host,postgres_port,postgres_user,postgres_password,postgres_database)
 
 kafka_bootstrap_server= config.get('kafka','bootstrap_servers').strip()
 kafka_group_id= config.get('kafka','group_id').strip()
 kafka_auto_offset_commit= config.get('kafka','auto_offset_reset').strip()
 kafka_enable_auto_commit= config.getboolean('kafka','enable_auto_commit')
+kafka_topic_name = config.get('kafka','topic_name')
+kafka_json_key = config.get('kafka','json_key')
 
 app = FastAPI()
 app.add_middleware(
@@ -42,7 +45,7 @@ conf = {'bootstrap.servers': kafka_bootstrap_server,
         'enable.auto.commit': kafka_enable_auto_commit,
         'client.id': socket.gethostname()}
 consumer = Consumer(conf)
-consumer.subscribe(config['kafka']['topic_name'])
+consumer.subscribe(kafka_topic_name)
 
 
 
@@ -53,7 +56,7 @@ def run_consumer_polling():
         msg_val = msg.value().decode('utf8')
         json_msg_val = json.loads(msg_val)
         try:
-            json_val = json_msg_val[config['kafka']['json_key']]
+            json_val = json_msg_val[kafka_json_key]
             print(json_val)
             cur.execute(f"INSERT INTO test_table_1(timestamp, test_val) VALUES(current_timestamp, '{json_val}');")
             conn.commit()
